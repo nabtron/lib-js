ae88c15e6a9fd819e0a8f50bf71e0ff081dbd452
nabtron <developer@nabtron.com>
2018-05-21 06:06:34 +0500

    Update smoothscroll-jquery.js

--- progressbar/smoothscroll-jquery.js
+++ progressbar/smoothscroll-jquery.js
@@ -1,5 +1,7 @@
 
 // working with jQuery 3.3.1
+// Adding this to the end of the file will enable smooth scroll to all on-page anchor links
+
 $(document).ready(function() {            
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
